ebad23d7982f117e6a62135a509834e59fa4176e
IgorMedvedev <psydov81@gmail.com>
2024-10-18 17:11:57 +0300

    hw #62

--- src/main/java/telran/monitoring/pulse/PulseSenderAppl.java
+++ src/main/java/telran/monitoring/pulse/PulseSenderAppl.java
@@ -1,97 +1,96 @@
 package telran.monitoring.pulse;
 
 import java.net.*;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.*;
 
 import telran.monitoring.pulse.dto.SensorData;
 
 public class PulseSenderAppl {
-	private static final int N_PACKETS = 100;
-	private static final long TIMEOUT = 500;
-	private static final int N_PATIENTS = 5;
-	private static final int MIN_PULSE_VALUE = 50;
-	private static final int MAX_PULSE_VALUE = 200;
-	private static final String HOST = "localhost";
-	private static final int PORT = 5000;
-	private static final int JUMP_PROB = 15;
-	private static final int MIN_JUMP_PERCENT = 10;
-	private static final int MAX_JUMP_PERCENT = 100;
-	private static final int JUMP_POSITIVE_PROB = 70;
-	private static final int PATIENT_ID_PRINTED_VALUES = 3;
-	private static Random random = new Random();
-	private static HashMap<Long, Integer> patientIdPulseValue = new HashMap<>();
-	static DatagramSocket socket;
 
-	public static void main(String[] args) throws Exception {
-		socket = new DatagramSocket();
-		IntStream.rangeClosed(1, N_PACKETS).forEach(PulseSenderAppl::sendPulse);
+    private static final int PORT = 5000;
+    private static final String HOST = "127.0.0.1";
+    private static final int N_PATIENTS = 1000;
+    private static final int MIN_PULSE_VALUE = 40;
+    private static final int MAX_PULSE_VALUE = 210;
+    private static final int WARN_MAX_PULSE_VALUE = 180;
+    private static final int WARN_MIN_PULSE_VALUE = 55;
+    private static final int MAX_ERROR_PULSE_PROB = 5;
+    private static final int MIN_ERROR_PULSE_PROB = 3;
+    private static final int JUMP_PROB = 2; 
+    private static final int INTERVAL_MILLIS = 1000;
 
-	}
+    private static final Logger logger = Logger.getLogger(PulseSenderAppl.class.getName());
 
-	static void sendPulse(int seqNumber) {
-		SensorData data = getRandomSensorData(seqNumber);
-		if (data.patientId() == PATIENT_ID_PRINTED_VALUES) {
-			System.out.printf("Pulse value of patient %d is %d\n", PATIENT_ID_PRINTED_VALUES, data.value());
-		}
-		String jsonData = data.toString();
-		sendDatagramPacket(jsonData);
-		try {
-			Thread.sleep(TIMEOUT);
-		} catch (InterruptedException e) {
+    private static DatagramSocket socket;
+    private static InetAddress address;
+    private static ThreadLocalRandom random = ThreadLocalRandom.current();
+    private static Map<Long, Integer> patientIdPulseValue = new HashMap<>();
 
-		}
-	}
-
-	private static void sendDatagramPacket(String jsonData) {
-		byte[] buffer = jsonData.getBytes();
-		try {
-			DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(HOST), PORT);
-			socket.send(packet);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-
-	}
-
-	private static SensorData getRandomSensorData(int seqNumber) {
-
-		long patientId = random.nextInt(1, N_PATIENTS + 1);
-		int value = getRandomPulseValue(patientId);
-		return new SensorData(seqNumber, patientId, value, System.currentTimeMillis());
-	}
-
-	private static int getRandomPulseValue(long patientId) {
-		int valueRes = patientIdPulseValue.computeIfAbsent(patientId,
-				k -> random.nextInt(MIN_PULSE_VALUE, MAX_PULSE_VALUE + 1));
-		if (chance(JUMP_PROB)) {
-			valueRes = getValueWithJump(valueRes);
-			patientIdPulseValue.put(patientId, valueRes);
-		}
-
-		return valueRes;
-	}
+    public static void main(String[] args) throws Exception {
+        configureLogging();
+        socket = new DatagramSocket();
+        address = InetAddress.getByName(HOST);
+        
+        int seqNumber = 0;  
+        
+        while (true) {
+            long patientId = getRandomPatientId();
+            int pulseValue = getRandomPulseValue(patientId);
+            seqNumber++;  
+            SensorData data = new SensorData(seqNumber, patientId, pulseValue, System.currentTimeMillis());
+            sendSensorData(data);
+            Thread.sleep(INTERVAL_MILLIS);
+        }
+    }
 
 
-	private static int getValueWithJump(int previousValue) {
-		int jumpPercent = random.nextInt(MIN_JUMP_PERCENT, MAX_JUMP_PERCENT + 1);
-		int jumpValue = previousValue * jumpPercent / 100;
-		if (!chance(JUMP_POSITIVE_PROB)) {
-			jumpValue = -jumpValue;
-		}
-		int res = previousValue + jumpValue;
-		if (res < MIN_PULSE_VALUE) {
-			res = MIN_PULSE_VALUE;
-		} else if (res > MAX_PULSE_VALUE) {
-			res = MAX_PULSE_VALUE;
-		}
-		return res;
-	}
+    private static void configureLogging() {
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.FINE);
+        Handler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.FINE);
+        logger.addHandler(consoleHandler);
+        
+        logger.config("Pulse Sender Application started.");
+    }
 
-	private static boolean chance(int prob) {
+    private static long getRandomPatientId() {
+        return random.nextLong(1, N_PATIENTS + 1);
+    }
 
-		return random.nextInt(0, 100) < prob;
-	}
+    private static int getRandomPulseValue(long patientId) {
+        int valueRes = patientIdPulseValue.computeIfAbsent(patientId,
+                k -> random.nextInt(MIN_PULSE_VALUE, MAX_PULSE_VALUE + 1));
 
+        if (chance(JUMP_PROB)) {
+            valueRes = getValueWithJump(valueRes);
+            patientIdPulseValue.put(patientId, valueRes);
+        }
+        
+        if (chance(MAX_ERROR_PULSE_PROB)) {
+            valueRes = MAX_PULSE_VALUE + 10;
+        } else if (chance(MIN_ERROR_PULSE_PROB)) {
+            valueRes = MIN_PULSE_VALUE - 10;
+        }
+
+        return valueRes;
+    }
+
+    private static boolean chance(int probability) {
+        return random.nextInt(100) < probability;
+    }
+
+    private static int getValueWithJump(int value) {
+        int delta = random.nextInt(5, 21); 
+        return random.nextBoolean() ? value + delta : value - delta;
+    }
+
+    private static void sendSensorData(SensorData data) throws Exception {
+        byte[] buffer = data.toString().getBytes();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, PORT);
+        socket.send(packet);
+        logger.fine("Sent SensorData: " + data);
+    }
 }
